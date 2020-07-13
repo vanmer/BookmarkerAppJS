278aa1e2d5fbe3c6b6116e843b694e6d8824fc2b
vanmer <60948275+vanmer@users.noreply.github.com>
2020-07-13 14:15:59 +0200

    styling changes, listen for form submit, savebookmark function, prevent form from submitting, localStorage test

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-// // Listen for form submit
-// document.getElementById("myForm").addEventListener("submit", saveBookmark);
+// Listen for form submit
+document.getElementById("myForm").addEventListener("submit", saveBookmark);
 
-console.log("hola!");
+// Save bookmark
+function saveBookmark(e) {
+  // Get form values
+  var siteName = document.getElementById("siteName").value;
+  var siteURL = document.getElementById("siteURL").value;
+
+  var bookmark = {
+    name: siteName,
+    url: siteURL
+  }
+
+  // // Local Storage Test
+  // localStorage.setItem('test', 'Hola chica!');
+  // console.log(localStorage.getItem('test'));
+  // localStorage.removeItem('test');
+  // console.log(localStorage.getItem('test'));
+
+  
+
+  // Prevent form from submitting
+  e.preventDefault();
+}
