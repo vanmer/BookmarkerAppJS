bbb3eb3e624ee227688f84c1bfb8b9317a1c8834
vanmer <60948275+vanmer@users.noreply.github.com>
2020-07-13 14:27:42 +0200

    setting and getting bookmarks from localStorage

--- js/main.js
+++ js/main.js
@@ -12,13 +12,30 @@ function saveBookmark(e) {
     url: siteURL
   }
 
-  // // Local Storage Test
-  // localStorage.setItem('test', 'Hola chica!');
-  // console.log(localStorage.getItem('test'));
-  // localStorage.removeItem('test');
-  // console.log(localStorage.getItem('test'));
+  /*
+    // Local Storage Test
+    localStorage.setItem('test', 'Hola chica!');
+    console.log(localStorage.getItem('test'));
+    localStorage.removeItem('test');
+    console.log(localStorage.getItem('test'));
+  */
 
-  
+  // Test if bookmark is null
+  if (localStorage.getItem('bookmarks') === null) {
+    // Init array
+    var bookmarks = [];
+    // Add to array
+    bookmarks.push(bookmark);
+    // Set to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
+    // Get bookmarks from localStorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // Add boomark to array
+    bookmarks.push(bookmark);
+    // Re-set back to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
 
   // Prevent form from submitting
   e.preventDefault();
